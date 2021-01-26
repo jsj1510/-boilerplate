162649769df9c67ca5626ae0650d29a067c491e6
jsj <tw450497@gmail.com>
2021-01-26 20:35:00 +0900

    implement:mongoDB,model&schema,bodyparser,nodemon

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+const config = require('./config/key');
+
+// app/x-www-form-urlencoded 분석
+app.use(bodyParser.urlencoded({extended: true}));
+// app/json타입분석
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+  useCreateIndex: true, 
+  useFindAndModify: false
+}).then(() => console.log('MongoDB Connected..')).catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!1')
+});
+
+app.post("/register", (req, res) => {
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  }) 
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
