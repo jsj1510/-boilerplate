31044805860e4a50ec43c60d30f2e155cff3f232
jsj <tw450497@gmail.com>
2021-02-04 13:06:37 +0900

    implement: antd css framwork, reducer

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers, conbineReducers } from 'redux';
+import user from './user_reducer';
+
+const rootReducer = combineReducers({
+    //user,
+})
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux';
+import promisMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers'
+
+const crateStoreWithMiddleware = applyMiddleware(promisMiddleware, ReduxThunk) (createStore)
 
 ReactDOM.render(
+  <Provider
+    store={crateStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )} >
     <App />
-  ,
-  document.getElementById('root')
+  </Provider>
+
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
